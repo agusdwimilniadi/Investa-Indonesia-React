955bcb95744e36c12688655d3f2adf5891603e56
Agus Dwi <agusdwimill@gmail.com>
2021-10-07 08:28:00 +0700

    a

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Mitra from "./components/mitra/Mitra";
 import Navbar from "./components/Navbar/Navbar";
 import Proyek from "./components/proyek/Proyek";
 import { useEffect } from "react";
-import ErrorPage from "./components/NotFound/NotFound";
 
 function App() {
   useEffect(() => {
@@ -31,7 +30,6 @@ function App() {
       <Route path="/mitra" exact component={Mitra} />
       <Route path="/edit/:id" exact component={EditMitra} />
       <Route path="/carakerja" exact component={CaraKerja} />
-      <Route component={ErrorPage} />
 
       <Footer />
     </>
